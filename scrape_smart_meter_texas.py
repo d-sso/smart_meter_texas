60a82cd0175dc2675023c4e215a5bd3ebc7475c9
valves <v.hudson.alves@gmail.com>
2025-03-27 15:08:26 -0500

    Improvements on error handling and logging

--- scrape_smart_meter_texas.py
+++ scrape_smart_meter_texas.py
@@ -38,14 +38,14 @@ class smt_handler:
 
             self.logger.debug('Logging in')
             wait = WebDriverWait(browser, config_variables.smart_meter_texas_sleep_timer_timeout_page_load)
-            browser.get_screenshot_as_file("BeforeLogin.png")
+            #browser.get_screenshot_as_file("BeforeLogin.png")
             wait.until(EC.element_to_be_clickable((By.ID, "userid"))).send_keys(username)
             wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(pwd)
-            browser.get_screenshot_as_file("AfterType.png")
+            #browser.get_screenshot_as_file("AfterType.png")
             wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginform"]/div[8]/button'))).click()
 
             time.sleep(config_variables.smart_meter_texas_sleep_timer_after_login)
-            browser.get_screenshot_as_file("AfterLogin.png")
+            #browser.get_screenshot_as_file("AfterLogin.png")
             self.logger.debug('Inside!')
             value = get_local_storage_item(browser, config_variables.smart_meter_texas_user_cookie)
             self.token = json.loads(value)['token']
@@ -56,9 +56,11 @@ class smt_handler:
             for cookie in cookies:
                 self.cookies[cookie['name']] = cookie['value']
         except Exception as e:
-            self.logger.error("Issue logging in! Screenshot will be captured")
             if config_variables.smart_meter_texas_error_screenshot_file:
+                self.logger.error("Issue logging in! Screenshot will be captured")
                 browser.get_screenshot_as_file(config_variables.smart_meter_texas_error_screenshot_file)
+            else:
+                self.logger.error(f"Issue logging in! {e}")
     
     def request_meter_read(self):
         request_headers = {
@@ -71,10 +73,10 @@ class smt_handler:
             , "MeterNumber": self.MeterNumber
         }
         try:
-            self.logger.info('Issuing read request')
+            self.logger.debug('Issuing read request')
             response = requests.post(config_variables.smart_meter_texas_on_demand_issue_read_api,data=json.dumps(request_data),headers=request_headers, timeout=30)
             if response.ok:
-                self.logger.info('Request meter read suceeded')
+                self.logger.info(f'Request meter read suceeded - {response.text}')
                 return True
             else:
                 self.logger.error(f"Request meter read failed - {response.text}")
@@ -98,13 +100,16 @@ class smt_handler:
             if response.ok:
                 val = response.json()
                 while val['data']['odrstatus'] == "PENDING":
-                    self.logger.info(f'Meter result not ready, retry in {config_variables.smart_meter_texas_sleep_after_read_request} seconds')
+                    self.logger.debug(f'Meter result not ready, retry in {config_variables.smart_meter_texas_sleep_after_read_request} seconds')
                     time.sleep(config_variables.smart_meter_texas_sleep_after_read_request)
                     response = requests.post(config_variables.smart_meter_texas_on_demand_get_read_api,data=json.dumps(request_data),headers=request_headers)
                     val = response.json()
-                self.logger.info('Received meter read')
+                self.logger.debug('Received meter read')
                 self.logger.info(val)
-                return val
+                # check if value returned has expected fields and is good. Instances of status equal to "none" and "error" caused issues
+                if val['data'] and val['data']['odrstatus'] and val['data']['odrread'] and val['data']['odrstatus'] == "COMPLETED":
+                    return val['data']['odrread']
+                return False
             else:
                 self.logger.error(f"Collect meter result failed - {response.text}")
                 return False
@@ -132,7 +137,7 @@ def main():
     while True:
         now_ts = datetime.datetime.now()
         now_hour = now_ts.hour
-        logger.info(f"Waking up to process - now ts = {now_ts}, last_run = {last_run}, next refresh = {last_run + datetime.timedelta(0,config_variables.smart_meter_texas_refresh_period)}")
+        logger.debug(f"Waking up to process - now ts = {now_ts}, last_run = {last_run}, next refresh = {last_run + datetime.timedelta(0,config_variables.smart_meter_texas_refresh_period)}")
         if (now_ts-last_run).total_seconds() > config_variables.smart_meter_texas_refresh_period:
             last_run = now_ts
             if((now_ts-handler.token_last_obtained).total_seconds() > config_variables.smart_meter_texas_login_token_refresh_period):
@@ -140,13 +145,17 @@ def main():
                 handler.login(username=config_variables.smart_meter_texas_user,pwd=config_variables.smart_meter_texas_pwd)
             if handler.request_meter_read():
                 val = handler.collect_meter_read()
-                meter_value = val['data']['odrread']
-                msg_info = mqttc.publish(config_variables.MQTT_info['target_topic'],meter_value,qos=0)
-                msg_info.wait_for_publish()
+                if(val):
+                    meter_value = val
+                    msg_info = mqttc.publish(config_variables.MQTT_info['target_topic'],meter_value,qos=0)
+                    msg_info.wait_for_publish()
+                    logger.info(f"Finished processing - next refresh = {last_run + datetime.timedelta(0,config_variables.smart_meter_texas_refresh_period)}")
+                else:
+                    logger.warning("Service returned bad data - not forwarded to MQTT")
             else:
                 logger.warning("Error requesting read, will try again next hour")
         
-        logger.info(f"Going to sleep for {config_variables.smart_meter_texas_sleep_between_cycles/60} min")
+        logger.debug(f"Going to sleep for {config_variables.smart_meter_texas_sleep_between_cycles/60} min")
         time.sleep(config_variables.smart_meter_texas_sleep_between_cycles)
 
 
